dd10c53eff054e9c9e0c8c8ff870bdb9a55768d1
jeremyfromearth <jeremynealbrown@gmail.com>
2017-12-04 17:48:06 -0800

    Added base Parser class and util methods

--- metonym/__init__.py
+++ metonym/__init__.py
@@ -1,4 +1,6 @@
 import re
+import string
+import itertools
 
 class Parser:
   def __init__(self):
@@ -6,11 +8,12 @@ class Parser:
     self.index = 0
     self.output = None
 
-  def go(self):
+  def go(self, input_str):
     """
     Invokes the creation of an abstract syntax tree through recursive decent parsing
     """
     self.index = 0
+    self.input = input_str
     self.output = self.expr() 
     return self.output
 
@@ -38,23 +41,43 @@ class Parser:
     pass
 
   # utility functions
-  def match(self, regex, expected_match_input=0):
+  def match(self, pattern):
     """
     Returns the result of a regex match at a specific index, None otherwise
     """
-    pass
+    if self.index < len(self.input):
+      m = re.match(pattern, self.input)
+      if m is not None:
+        value = m.group(0)
+        self.index += len(value)
+        return value
+    return None
 
   def char(self, c):
     """
     Return a boolean indicating whether or not the next character in the input matches the supplied parameter
     """
-    pass
+    if self.index < len(self.input):
+      if self.input[self.index] == c:
+        self.index += 1
+        return True
+    return False
 
   def ignore_whitespace(self):
     """
     Advances index until a non whitespace character is reached
     """
-    pass
+    while self.index < len(self.input):
+      if self.input[self.index] in string.whitespace:
+        self.index += 1
+      else:
+        break
+
+  def collapse(self, l):
+    """
+    Collapse a 2 dimensional list into a 1 dimensional list
+    """
+    return list(itertools.chain.from_iterable(l))
 
 class MetonymParser(Parser):
   def __init__(self):
@@ -64,3 +87,9 @@ class MetonymParser(Parser):
     node = None
     return node
 
+
+if __name__ == '__main__':
+  p = Parser()
+  p.go(' hello world')
+  p.char('')
+  print(p.index)
